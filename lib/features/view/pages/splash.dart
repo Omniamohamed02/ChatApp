2aa468eb1fc86cb52d6844356ceb9ad56e9af282
Omniamohamed02 <omnia.mohamed.official02@gmail.com>
2024-10-04 17:50:05 +0300

    delete button

---
+++ lib/features/view/pages/splash.dart
@@ -0,0 +1,45 @@
+import 'package:chatapp/features/view/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    Future.delayed(Duration(seconds: 3), () {
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+
+    return Scaffold(
+      body:   Center(
+        child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                 Image.asset('lib/core/constant/assets/images/logo.png',height: 200,width: 200,),
+                const SizedBox(height: 30),
+                const Text(
+                  "what's App",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 30),
+               const Text(
+                  'The best chat app of this contury',
+                  style: TextStyle(fontSize: 16),
+                ),
+
+              ],
+            ),
+          ),
+      ),
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:chatapp/view/pages/splash.dart';
+
 import 'package:flutter/material.dart';
+import 'features/view/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
      debugShowCheckedModeBanner: false,
       home: Splash(),
     );
