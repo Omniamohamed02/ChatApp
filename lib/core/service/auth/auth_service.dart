e36849c6dacf276ddccf1ee7017b05b245d51323
Omniamohamed02 <omnia.mohamed.official02@gmail.com>
2024-10-14 23:54:52 +0300

    update master

---
+++ lib/core/service/auth/auth_service.dart
@@ -0,0 +1,49 @@
+
+import 'package:chatapp/features/view/pages/chat_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+
+class AuthService{
+
+  void login(BuildContext context,String email,String password) async{
+    try{
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email, password: password);
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) =>  ChatPage(),
+        ),
+      );
+    } on FirebaseAuthException catch(e){
+      Navigator.pop(context);
+    }catch(e){
+      Navigator.pop(context);
+    }
+  }
+
+
+  void register(BuildContext context,String email,String password,String confirmPassword) async {
+
+    try {
+      if(password == confirmPassword) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: email, password: password);
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) =>  ChatPage(),
+          ),
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+    } catch (e) {
+      Navigator.pop(context);
+    }
+  }
+
+
+  void logout(){
+    FirebaseAuth.instance.signOut();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'features/view/pages/splash.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
